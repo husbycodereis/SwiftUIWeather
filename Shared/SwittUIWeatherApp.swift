ff98bf17f04e8f4011c42f9a93b9363aa8d8922e
Ali Rıza Reisoğlu <alirizareisoglu@gmail.com>
2022-05-24 15:55:34 +0300

    Initial Commit

---
+++ Shared/SwittUIWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwittUIWeatherApp.swift
+//  Shared
+//
+//  Created by Ali Riza Reisoglu on 24.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwittUIWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
